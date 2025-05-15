764cf9c513fb1bb8a18f812f3f817c7bd8d191fa
Aaron  Macken <aaronmacken@Mac.hsd1.ga.comcast.net>
2025-05-15 19:28:31 -0400

    Initial input validation example

--- src/components/Application/Application.tsx
+++ src/components/Application/Application.tsx
@@ -1,5 +1,10 @@
+import InputValidation from '@components/examples/InputValidation';
 import * as styles from './Application.scss';
 
 export default function Application() {
-  return <h1 className={styles.root}>This is my application!</h1>;
+  return (
+    <div className={styles.root}>
+      <InputValidation />
+    </div>
+  );
 }
