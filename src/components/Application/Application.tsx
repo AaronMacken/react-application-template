79a2ccb332f4c57e9725ee924207304b3ce3b92e
aaronmackengithub <aaronmacken1@gmail.com>
2025-08-16 11:55:49 -0400

    Updated hooks alias for webpack / TS config | created custom hook for react form example

--- src/components/Application/Application.tsx
+++ src/components/Application/Application.tsx
@@ -1,9 +1,12 @@
+import Form from '@components/examples/Form';
 import * as styles from './Application.scss';
 
 export default function Application() {
   return (
     <div className={styles.root}>
       <h1>Testing</h1>
+      <br />
+      <Form />
     </div>
   );
 }
