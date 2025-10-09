220a92662a02e9e7302f6cba49944ea5f87e75ef
Aaron <aaron.a.macken@gmail.com>
2025-10-08 20:09:10 -0400

    Fisher yates, and Indecies of Sum

--- src/components/Sandbox/Sandbox.tsx
+++ src/components/Sandbox/Sandbox.tsx
@@ -1,23 +1,25 @@
 import { useEffect } from 'react';
 import { useTheme } from '@context';
-import ProductForm from '@components/examples/ProductForm';
+// import ProductForm from '@components/examples/ProductForm';
 // import Counter from '@components/examples/Counter';
 // import Form from '@components/examples/Form';
+import { getIndeciesOfSum } from '@components/examples/Algorithms';
 import { getIsDark } from '@utils';
 import * as styles from './Sandbox.scss';
 
-// https://www.youtube.com/watch?v=uDCBSnWkuH0&t=879s
 const Sandbox = () => {
   const { theme } = useTheme();
-
   const cls = getIsDark(theme) ? 'darkThemeStyles' : 'lightThemeStyles';
+
+  useEffect(() => {
+    const indecies = getIndeciesOfSum([5, 3, 1, 6], 55);
+
+    console.log('INDECIES: ', indecies);
+  }, []);
 
   return (
     <div className={styles[cls]}>
       <p>sandbox</p>
-      <ProductForm />
-      {/* <Counter /> */}
-      {/* <Form theme={theme} /> */}
     </div>
   );
 };
